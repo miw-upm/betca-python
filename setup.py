05dccc1eef50578e870a9f6106b6f4365c529ac3
jbernal <j.bernal@upm.es>
2021-03-11 16:05:13 +0100

    version 1.1.0-SNAPSHOT

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="betca-python-beginning",
-    version="1.0.0",
+    version="1.1.0-SNAPSHOT",
     description="Beginning",
     author="Jesus Bernal",
     packages=find_packages(),
